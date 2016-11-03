b73f5fc7a889b21358ced73a2d1be801c8b1291a
Totta <totta@harriet.biologie.hu-berlin.de>
2016-11-03 16:12:02 +0100

    Adjustments in code for and renaming of figure 1a and 1b

--- 0_oocystCounts_and_qPCR.R
+++ 0_oocystCounts_and_qPCR.R
@@ -17,58 +17,42 @@ phen.data <- read.csv("data/Oocysts_output_weight_SS_longdata.csv")
 ## here just a hack to take the first occurence of a ID at a time point
 phen.data <- phen.data[!duplicated(phen.data[, c("Day_pi", "Mouse_ID", "Infection_No")]), ]
 
-#all.oocysts.line <-
-#    ggplot(phen.data,
-#           aes(Day_pi, Oocysts_feces, group=Mouse_ID, color=Mouse_strain)) +
-#    facet_wrap(~Infection_No)+
-#    geom_line() 
-
-all.oocysts.line <-
-  ggplot(phen.data,
-         aes(Day_pi, Oocysts_feces, color=Mouse_strain)) +
-  facet_wrap(~Infection_No)
-
-  #geom_line() 
-
-
 oocyst.summary <- ddply(phen.data, c("Mouse_strain", "Day_pi", "Infection_No"), summarize,
-                      N    =  sum(!is.na(Oocysts_feces)),
-                      Cmean = mean(Oocysts_feces, na.rm=TRUE),
-                      Csd =   sd(Oocysts_feces, na.rm=TRUE),
-                      Cse =   Csd/sqrt(N))
+                        N    =  sum(!is.na(Oocysts_feces)),
+                        Cmean = mean(Oocysts_feces, na.rm=TRUE),
+                        Csd =   sd(Oocysts_feces, na.rm=TRUE),
+                        Cse =   Csd/sqrt(N))
 
-## create colors for plot
+## create colors for Figure 1a
 palette.colors <- brewer.pal(8, "Dark2")
 my.colors.ooc <- palette.colors[c(1,2,8)]
+#pdf("figures/Figure1a_oocystCounts.pdf", width=12, height=8)
 
-pdf("figures/Figure1a_oocystCounts.pdf", width=12, height=8)
-png("figures/Figure1a_oocystCounts.png", width=1200, height=800)
-all.oocysts.line +
-    geom_point(data=oocyst.summary, 
-               aes(Day_pi, Cmean)) +
-    geom_line(data=oocyst.summary, 
-              aes(Day_pi, Cmean),
-              size = 0.7) +
-    geom_errorbar(data=oocyst.summary,
-                  aes(x=Day_pi, y=Cmean, #group=NULL,
-                      ymin=Cmean-Cse, 
-                      ymax=Cmean+Cse), 
-                  size=1,
-                  width = 0.3) +
-    scale_color_manual(values = my.colors.ooc) +
-    scale_y_continuous("Oocyst number in feces", 
-                     labels = comma, 
-                     breaks = c(0, 1000000, 2000000, 3000000, 4000000)) +
-    scale_x_continuous("Day post infection",
-                       breaks = c(0, 3, 5, 7, 9, 11, 13, 15)) + 
-    theme_bw(20) +
-    theme(legend.key = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-    ggtitle("Oocyst counts in first and second infection")
-## not saving the latter part - only part creating the plot   !!!!!!!!!!!!!!!!!!!
-#ggsave(file = "figures/Figure1a_oocystsCounts.svg", plot = all.oocysts.line)
-dev.off()
+#labels for facet grid for Figure 1a
+my.labels <- c("1" = "1st infection", "2" = "2nd infection")
+
+## Plotting Figure 1a
+all.oocysts.line <- ggplot(phen.data,
+         aes(Day_pi, Oocysts_feces, color=Mouse_strain)) +
+  facet_wrap(~Infection_No, labeller = labeller(Infection_No = my.labels)) + 
+  geom_point(data=oocyst.summary, aes(Day_pi, Cmean)) +
+  geom_line(data=oocyst.summary, aes(Day_pi, Cmean), size = 0.7) +
+  geom_errorbar(data=oocyst.summary, aes(x=Day_pi, y=Cmean, ymin=Cmean-Cse, ymax=Cmean+Cse), 
+                size=1,
+                width = 0.3) +
+  scale_color_manual(values = my.colors.ooc) +
+  scale_y_continuous("Oocyst number in feces", 
+                   labels = comma, 
+                   breaks = c(0, 1000000, 2000000, 3000000, 4000000)) +
+  scale_x_continuous("Day post infection",
+                     breaks = c(0, 3, 5, 7, 9, 11, 13, 15)) + 
+  theme_bw(20) +
+  theme(legend.key = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank())
+  #ggtitle("Oocyst counts in first and second infection")
+ggsave(file = "figures/Figure1a_oocystsCounts.svg", height = 8, width = 12, plot = all.oocysts.line)
+#dev.off()
 
 ## qPCR data from Simone and Annica
 ## Ef18S qPCR data for different timepoints, 1st and 2nd infection and 3 biological replicates.
@@ -86,28 +70,27 @@ stats.qpcr <- merge(stats.qpcr, long.qpcr)
 
 ################ STATUS ####################
 ## Cool that we have so much qPCR data: find those genes in RNAseq data and 
+## plot together?
 
-
-## plot together
-## create colors for plot
+## create colors for Figure 1b
 palette.colors2 <- brewer.pal(11, "BrBG")
 my.colors.qpcr <- palette.colors2[c(3,9)]
+my.ylab <- expression(paste(italic("Eimeria"), " 18S fold change by qPCR"))
 
-##### plot goves error message due to my.colors code line, but plot looks fine...
-#devSVG(file = "figures/Figure1c_qPCR18S.svg")
+##### Plotting figure 1b
 qpcr18S <- ggplot(subset(stats.qpcr, stats.qpcr$gene %in% "Ef18S"), 
        aes(x = dpi, y = norm.value.mean, col = factor(inf))) +
-  ggtitle("Eimeria 18S by qPCR") +
+  #ggtitle("Eimeria 18S by qPCR") +
   labs(color = "Infection No.") +
   ## to get rid of label, rm aes(), but then size is not controlled. How fix this? ################ HERE ####################
   geom_errorbar(aes(ymin = norm.value.mean - norm.value.sd,
                     ymax = norm.value.mean + norm.value.sd,
                     width = 0.3)) +
-  geom_point(aes(alpha = 0.7)) +
-  geom_line(size = 0.7) +
+  geom_point() + #aes(alpha = 0.7)) +
+  geom_line(size = 1) +
   scale_color_manual(values = my.colors.qpcr) +
   ##  (delta-delta-ct): put in figure legend
-  scale_y_continuous("Eimeria 18S fold change", 
+  scale_y_continuous(my.ylab, 
                      labels = math_format(2^.x), 
                      breaks = c(0, 2, 4, 8, 16, 32),
                      limits = c(0, 32)) +
@@ -116,8 +99,8 @@ qpcr18S <- ggplot(subset(stats.qpcr, stats.qpcr$gene %in% "Ef18S"),
   theme(legend.key = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
-ggsave(file = "figures/Figure1c_qPCR18S.svg", plot = qpcr18S)
-dev.off()
+ggsave(file = "figures/Figure1b_qPCR18S.svg", height = 12, width = 11, plot = qpcr18S)
+#dev.off()
 
 
 
