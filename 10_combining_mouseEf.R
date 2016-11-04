977c66b2e2c63264adac129e19200eb661f5d3c5
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2016-11-04 14:32:43 +0100

    Loading the RnB data only... after saving and commenting the procedure

--- 10_combining_mouseEf.R
+++ 10_combining_mouseEf.R
@@ -8,6 +8,74 @@ Ef.nRC <- read.table("output_data/Ef_norm_counts.csv", sep=",")
 
 both.col <- intersect(colnames(Mm.nRC), colnames(Ef.nRC))
 
+## Reid and Berriman approach: We randomized all profiles and
+## calculated the Pearson correlation coefficient between every
+## interspecific pair of genes. We repeated this 10^5 times and
+## calculated the P-value as the number of times a randomized pair of
+## genes profiles were correlated at least as well as the real
+## profiles, divided by 10^5
+
+## Cors <- cor(t(Mm.nRC[, both.col]), t(Ef.nRC[, both.col]))
+
+## registerDoParallel(cores=20)
+
+## outer.reps <- 5000
+## inner.reps <- 100
+
+## ## sum up parallel the numbers the random correlation values are
+## ## bigger than the real correlations
+## parallel.RnB <- function(inner.reps){
+##     randCors <-
+##         foreach(icount(inner.reps), .combine="+") %dopar% {
+##             rand.col <- sample(both.col, length(both.col))
+##             RC <- cor(t(Mm.nRC[, rand.col]),
+##                       t(Ef.nRC[, both.col]))        
+##             res <- abs(RC)>abs(Cors)
+##             ## avoid an accumulation of NAs for In cor(t(Mm.nRC[,
+##             ## both.col]), t(Ef.nRC[, both.col])) : the standard
+##             ## deviation is zero
+##             res[is.na(res)] <- TRUE
+##             return(res)
+##         }
+##     return(randCors)
+## }
+
+
+## ## a large number of non-parallelized chunks. Putting everything in
+## ## the parallel function would eat up memory
+## RnB <- matrix(0, nrow=nrow(Cors), ncol=ncol(Cors))
+## for(i in 1:outer.reps){
+##     ## sum each result with what has been there before
+##     RnB.new <- parallel.RnB(inner.reps)
+##     print(paste("finished outer rep", i))
+##     RnB <- RnB + RnB.new
+## }
+
+## file.RnB <- paste("//home/ele/RnB_",
+##                   round(as.vector(Sys.time())), ".Rdata", sep="")
+
+## save(RnB, file = file.RnB)
+
+
+## RnB.final <- RnB / (outer.reps*inner.reps)
+
+## prod.file.RnB <- paste("/home/ele/RnB_Prod_",
+##                        round(as.vector(Sys.time())), ".Rdata", sep="")
+
+## save(RnB.final, file = prod.file.RnB)
+
+load("output_data/RnB_1478263654.Rdata")
+load("output_data/RnB_Prod_1478263755.Rdata")
+
+
+is.zero <- (RnB.final==0)
+
+is.zero.all.cols <- apply(is.zero, 2, any)
+Ef.genes.ineracting <- colnames(RnB)[is.zero.all.cols]
+
+is.zero.all.rows <- apply(is.zero, 1, any)
+Mm.genes.ineracting <- rownames(RnB)[is.zero.all.rows]
+
 ## nRC <- rbind(Mm.nRC[, both.col], Ef.nRC[, both.col])
 
 ## get.scaled.and.clustered <- function(data){
@@ -51,76 +119,3 @@ both.col <- intersect(colnames(Mm.nRC), colnames(Ef.nRC))
 ##          cutree_rows = 7, 
 ##          show_rownames = F
 ##          )
-
-## Reid and Berriman approach: We randomized all profiles and
-## calculated the Pearson correlation coefficient between every
-## interspecific pair of genes. We repeated this 10^5 times and
-## calculated the P-value as the number of times a randomized pair of
-## genes profiles were correlated at least as well as the real
-## profiles, divided by 10^5
-
-Cors <- cor(t(Mm.nRC[, both.col]), t(Ef.nRC[, both.col]))
-
-registerDoParallel(cores=20)
-
-outer.reps <- 5000
-inner.reps <- 100
-
-## uncomment for testing
-## outer.reps <- 10
-## inner.reps <- 20
-
-## sum up parallel the numbers the random correlation values are
-## bigger than the real correlations
-parallel.RnB <- function(inner.reps){
-    randCors <-
-        foreach(icount(inner.reps), .combine="+") %dopar% {
-            rand.col <- sample(both.col, length(both.col))
-            RC <- cor(t(Mm.nRC[, rand.col]),
-                      t(Ef.nRC[, both.col]))        
-            res <- abs(RC)>abs(Cors)
-            ## avoid an accumulation of NAs for In cor(t(Mm.nRC[,
-            ## both.col]), t(Ef.nRC[, both.col])) : the standard
-            ## deviation is zero
-            res[is.na(res)] <- TRUE
-            return(res)
-        }
-    return(randCors)
-}
-
-
-## a large number of non-parallelized chunks. Putting everything in
-## the parallel function would eat up memory
-RnB <- matrix(0, nrow=nrow(Cors), ncol=ncol(Cors))
-for(i in 1:outer.reps){
-    ## sum each result with what has been there before
-    RnB.new <- parallel.RnB(inner.reps)
-    print(paste("finished outer rep", i))
-    RnB <- RnB + RnB.new
-}
-
-
-file.RnB <- paste("//home/ele/RnB_",
-                  round(as.vector(Sys.time())), ".Rdata", sep="")
-
-save(RnB, file = file.RnB)
-
-
-RnB.final <- RnB / (outer.reps*inner.reps)
-
-prod.file.RnB <- paste("/home/ele/RnB_Prod_",
-                       round(as.vector(Sys.time())), ".Rdata", sep="")
-
-save(RnB.final, file = prod.file.RnB)
-
-## all.x <- apply(RnB.final, 2, function(x) sum(x==0))
-
-foo <- (RnB.final==0)
-
-bar <- apply(foo, 2, any)
-Ef.genes.ineracting <- colnames(RnB)[bar]
-
-baz <- apply(foo, 1, any)
-Mm.genes.ineracting <- rownames(RnB)[baz]
-
-
